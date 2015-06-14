7c30172af21c688653127353faa5f5ace7ec0def
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-06-14 21:14:25 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,6 @@
 # about: You can insert PayPal «Buy Now» button to your forum posts.
 # version: 1.0.0
 # authors: Dmitry Fedyuk
-# url: http://discourse.pro/t/29
+# url: https://discourse.pro/t/29
 register_asset 'javascripts/server.js.erb', :server_side
 register_asset 'stylesheets/main.scss'
