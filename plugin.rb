6680587966e6eb2968c9f7455a85edc7a259a064
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-07-19 04:49:57 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -3,5 +3,5 @@
 # version: 2.0.0
 # authors: Dmitry Fedyuk
 # url: https://discourse.pro/t/29
-register_asset 'javascripts/dialect.js', :server_side
+register_asset 'javascripts/markdown.js', :server_side
 register_asset 'stylesheets/main.scss'
